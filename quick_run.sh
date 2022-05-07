3a82fcb5dcc7b38688eb24577e94c4e5b2332758
Bob Pepers <bob.pepers@gmail.com>
2022-05-07 05:38:23 +0200

    quick_run

--- quick_run.sh
+++ quick_run.sh
@@ -6,12 +6,12 @@
 
 # configure electrumx
 export COIN=Runebase
-export DAEMON_URL=http://rpcuser:rpcpassword@127.0.0.1
+export DAEMON_URL=http://runebaseinfo:runebaseinfo@127.0.0.1
 export NET=mainnet
 export DB_DIRECTORY=$HOME/.electrumx/db
-export SSL_CERTFILE=$HOME/.electrumx/certfile.crt
-export SSL_KEYFILE=$HOME/.electrumx/keyfile.key
-export SERVICES=tcp://127.0.0.1:50001
+export SSL_CERTFILE=$HOME/.electrumx/server.crt
+export SSL_KEYFILE=$HOME/.electrumx/server.key
+export SERVICES=tcp://:50001,ssl://:50002,wss://:50004,rpc://
 ulimit -n 65535
 
 
